7bf377af91ac0b0062f1e92167eb2715adfd5895
Amrit Shekhar Rana <59337133+AmritShekhar@users.noreply.github.com>
2021-06-11 10:14:20 +0530

    Add files via upload

---
+++ CSE-C/Amrit-1018660/q1_hello.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+#define ll                  long long
+#define endl                '\n'
+#define v(a)                vector<a>
+#define pb                  push_back
+#define loop(i,a,b)         for(ll (i)=(a);i<=(b);i++)
+#define rloop(i,a,b)        for(ll (i)=(a);i>=(b);i--)
+
+signed main() {
+
+    cout << "hello world" << endl;
+
+    return 0;
+}
---
+++ CSE-C/Amrit-1018660/q2_print.cpp
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+#define ll                  long long
+#define endl                '\n'
+#define v(a)                vector<a>
+#define pb                  push_back
+#define loop(i,a,b)         for(ll (i)=(a);i<=(b);i++)
+#define rloop(i,a,b)        for(ll (i)=(a);i>=(b);i--)
+
+signed main() {
+
+    loop(i,1,10) 
+        cout << i << endl;
+
+    return 0;
+}
