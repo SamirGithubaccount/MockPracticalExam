f8c5bcc4f8cba6108393f861097579a6c1ca4d41
Priyanshi Binjola <79206245+priyanshibinjola@users.noreply.github.com>
2021-06-11 10:11:25 +0530

    Add files via upload

---
+++ CSE-A/priyanshi binjola/2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class numbers
+{
+   public static void main(String args[])
+    {
+        int i;
+        for(i=1;i<=10;i++)
+         {
+              System.out.println(i);
+          }
+      }
+}
