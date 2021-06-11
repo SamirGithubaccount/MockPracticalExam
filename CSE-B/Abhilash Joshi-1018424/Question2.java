a3b6d42d35550c5d4885657e3e0dc79561b9f4e2
Abhilash Joshi <45088116+alphaspear@users.noreply.github.com>
2021-06-10 22:14:54 -0700

    Add files via upload

---
+++ CSE-B/Abhilash Joshi-1018424/Question2.java
@@ -0,0 +1,10 @@
+class numbers//Created Class Question2
+{
+    public static void main(String[] args)
+    {
+        for(int i = 1; i <= 10; i++)//Using For loop from 1 to 10
+        {
+            System.out.println(i);//pring numbers on screen
+        }
+    }
+}
