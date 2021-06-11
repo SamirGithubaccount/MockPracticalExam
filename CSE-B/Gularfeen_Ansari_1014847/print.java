bf4eb1b7d2333028ec77afcd9766f9f2efe1095d
gularfeenansari <53817870+gularfeenansari@users.noreply.github.com>
2021-06-11 10:11:24 +0530

    Add files via upload

---
+++ CSE-B/Gularfeen_Ansari_1014847/print.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class print {
+    public static void main(String arg[]) {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+        
+    }
+    
+}
